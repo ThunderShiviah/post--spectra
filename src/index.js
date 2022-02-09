05e187f6f1609785a27aadd847d1bd940bd97486
Thunder Shiviah <thunder.shiviah@spiden.com>
2022-02-09 18:49:11 +0100

    more port stuff

--- src/index.js
+++ src/index.js
@@ -11,9 +11,12 @@
 // 	const target = exampleTag.querySelector("#svelte-example-target");
 // 	example = new Example({ target });
 // });
-const PORT = process.env.PORT || 80;
-var server = app.listen(PORT, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("server is listening at http://%s:%s", host, port);
-});
+// const PORT = process.env.PORT || 80;
+// var server = app.listen(PORT, function() {
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log("server is listening at http://%s:%s", host, port);
+// });
+app.listen(process.env.PORT || 3000, function(){
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
